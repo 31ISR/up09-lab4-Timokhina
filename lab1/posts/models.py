a8c45bccfe6746c22e4334f247d75e57d77f8c52
Timokhina Sofya <sosyenka@gmail.com>
2025-02-06 22:52:35 +0600

    остановилась на 4-м* пункте..

--- lab1/posts/models.py
+++ lab1/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=75)
@@ -9,3 +10,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+author = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
